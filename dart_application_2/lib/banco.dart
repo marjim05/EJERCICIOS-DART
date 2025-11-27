a2d75775a5f0174bbf5003ccfc92ee3363bb6f2d
Marcela Mosquera <march.jim.05@gmail.com>
2025-11-27 08:34:54 -0500

    Add files via upload

---
+++ dart_application_2/lib/banco.dart
@@ -0,0 +1,11 @@
+void main () {
+  var capital = 1000000;
+  var razon = 0.02;
+
+  var totalCapital = (capital * razon) + capital;
+  print("El total del capital despues de un mes es: $totalCapital");
+}
+
+void sumar (int numero1) {
+  
+}
---
+++ dart_application_2/lib/cambioDivisas.dart
@@ -0,0 +1,32 @@
+import 'dart:math';
+
+void main() {
+  chelinesAPesetas();
+  dracmasAFrancos();
+  pesetasADolaresYLiras();
+
+}
+
+void chelinesAPesetas() {
+  var chelinesPesetas = 100 / 956871;
+  var chelines = 50;
+  var pesetas = chelines * chelinesPesetas;
+  print("El valor de $chelines chelines a pesetas es: $pesetas");
+}
+
+void dracmasAFrancos() {
+  var dracmasPesetas = 100 / 88607;
+  var dracmas = 45;
+  var dracmasTotal = dracmasPesetas * dracmas;
+  int francos = Random().nextInt(100);
+  var conversionFrancos = francos * dracmas;
+  print("El valor dracmas $dracmas dracmas a francos franceses es: $conversionFrancos ");
+}
+
+
+void pesetasADolaresYLiras (){
+  int pesetas = 45;
+  double dolares = pesetas / 122499;
+  double liras = pesetas / 0.09289;
+  print("El valo de $pesetas pesetas a dolares es: $dolares y a liras es: $liras");
+}
